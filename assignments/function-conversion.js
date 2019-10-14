a8150fee558ca515d5e85b6f2f2a6170aa833711
Jack Power <jackpower@Jacks-MacBook-Air.local>
2019-10-14 12:53:12 +0100

    Complete function-conversion section

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -4,21 +4,29 @@
 // console.log("Function was invoked!");
 // };
 // myFunction();
+myFunction = () => "Function was invoked!";
+console.log(myFunction);
 
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+anotherFunction = (param) => "Example";
+console.log(anotherFunction);
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
+add = (param1, param2) => param1 + param2;
+console.log(add(1,2));
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
+subtract = (param1, param2) => param1 - param2;
+console.log(subtract(2,1));
 
 
 // Stretch
